8365e9c2540382b00afa1cf84a6ed21eb3dace7b
dhrupatel1037 <dhrupatel@Dhrus-MacBook-Pro.local>
2017-02-18 18:46:37 -0500

    upload

---
+++ dhru/js/table.js
@@ -0,0 +1,117 @@
+var day = [0,1,2,3,4,5,6]
+var hour = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+var minute = [0,15,30,45];
+
+var freeTimes={
+  var n = new Date();
+  var c = n.getDay;
+
+
+
+
+  "B118": [
+    {
+      "start": {
+        "day": day[1],
+        "hour": hour[8],
+        "minute": minute[0],
+
+      }"end":{
+        "day": day[1],
+        "hour": hour[8],
+        "minute": minute[3],
+      }"start":{
+        "day": day[1],
+        "hour": hour[13],
+        "minute": minute[0],
+      }"end":{
+        "day": day[1],
+        "hour": hour[15],
+        "minute": minute[0],
+
+
+      }"start":{
+        "day": day[1],
+        "hour": hour[17],
+        "minute": minute[1],
+    }"end":{
+        "day": day[1],
+        "hour": hour[18]
+        "minute": minute[0]
+    }"start":{
+        "day": day[1],
+        "hour": hour[20],
+        "minute": minute[2],
+    }"end":{
+        "day": day[1],
+        "hour": hour[21],
+        "minute": minute[3],
+    }  "start":{
+        "day":day[2],
+        "hour": hour[10],
+        "minute": minute[0],
+    }"end":{
+        "day":day[2],
+        "hour":hour[10],
+        "minute": minute[3],
+    }"start":{
+      "day":day[2],
+      "hour":hour[17],
+      "minute":[0],
+    }"end":{
+      "day":day[2],
+      "hour":hour[21],
+      "minute":minute[3],
+    }"start":{
+      "day": day[3],
+      "hour": hour[8],
+      "minute": minute[0],
+    }"end":{
+      "day": day[3],
+      "hour": hour[8],
+      "minute": minute[3],
+    }"start":{
+      "day":day[3],
+      "hour":hour[12],
+      "minute":minute[0],
+    }"end":{
+      "day": day[3],
+      "hour": hour[18],
+      "minute": minute[0],
+    }"start":{
+      "day":day[3],
+      "hour": hour[20],
+      "minute": minute[3],
+
+    }"end":{
+      "day": day[3],
+      "hour": hour[21],
+      "minute": minute[3],
+    }
+  ]
+
+
+}
+
+function getTime(){
+
+  var d = new Date(); // for now
+  d.getDay();
+  d.getHours(); // => 9
+  d.getMinutes();
+
+
+}
+
+function compareDay(){
+
+  var d = new Date(); // for now
+  d.getDay();
+
+
+  if (d.getDay()=="Mon"){
+    if (d.getHours()==8 && d.getMinutes()<50){
+
+    }
+  }
+}
